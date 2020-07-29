c1ea5756d558bb9b89770cad5a9be6315bf91acf
juny5131 <juny5131@naver.com>
2020-07-29 13:18:53 +0900

    1st commit

---
+++ src/main/java/demo/Shipped.java
@@ -0,0 +1,34 @@
+package demo;
+
+public class Shipped extends AbstractEvent {
+
+    private Long id;
+    private Long orderid;
+    private String status;
+
+    public Shipped(){
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getOrderid() {
+        return orderid;
+    }
+
+    public void setOrderid(Long orderid) {
+        this.orderid = orderid;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
